115da140497ee34bfe55bcc0d50177111bed2e5d
Benoit Frisque <benoitfrisque@gmail.com>
2025-01-02 16:28:06 +0100

    added pie chart

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -35,6 +35,7 @@ df_resampled = (
     .resample("1D")
     .mean()
     .interpolate(method="linear")
+    .round(2)
     .reset_index()
 )
 
@@ -42,32 +43,63 @@ df_resampled = (
 df_resampled = df_resampled.merge(accounts, on="account")
 
 # Stacked area plot
-fig = px.area(
+COLOR_PALETTE = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"]
+BODY_FONT_FAMILY = "'Source Sans Pro', sans-serif"
+PLOTLY_FONT_COLOR = "#545b61" # Grey
+LEGEND_BORDER_COLOR = "#dee2e6" # Light grey
+
+
+# Stacked area plot
+fig_area = px.area(
     df_resampled,
     x="date",
     y="balance",
     color="type",
     line_group="account",
-    category_orders={"type": ["checking", "savings", "stocks", "crypto"]},
-    title="Total Portfolio Value Over Time",
+    category_orders={"type":  ['checking', 'savings', 'stocks', 'crypto']},
     template="plotly_white",
     color_discrete_sequence=COLOR_PALETTE,
 )
 
-fig.update_layout(
-    xaxis_title="Date",
+fig_area.update_layout(
+    xaxis_title="",
     yaxis_title="Balance",
-    yaxis_ticksuffix="€",
+    yaxis_ticksuffix='€',
     plot_bgcolor="rgba(0, 0, 0, 0)",
     paper_bgcolor="rgba(0, 0, 0, 0)",
     font_color=PLOTLY_FONT_COLOR,
     font=dict(family=BODY_FONT_FAMILY),
-    hoverlabel=dict(
-        font=dict(
-            family=BODY_FONT_FAMILY,
-            color=PLOTLY_FONT_COLOR
-            )
-        ),
+    hoverlabel=dict(font=dict(family=BODY_FONT_FAMILY, color=PLOTLY_FONT_COLOR)),
+    margin=dict(l=0, r=0, t=50, b=0),
+    legend=dict(
+        title="Account Type",
+        orientation="h",  # Horizontal orientation
+        x=0.5,            # Center horizontally
+        y=1,            # Position above the plot area
+        xanchor="center", # Align center of the legend
+        yanchor="bottom", # Align bottom of the legend
+    ),
+)
+
+# Selected date for sunburst plot (to be changed to be interactive)
+selected_date = pd.to_datetime("10/09/2024", format='%d/%m/%Y')
+df_selected_date = df_resampled[df_resampled["date"] == selected_date].sort_values(['type', 'balance'])
+
+fig_pie = px.sunburst(
+    df_selected_date,
+    path=["type", "account"],
+    values="balance",
+    template="plotly_white",
+    color_discrete_sequence=COLOR_PALETTE,
+)
+
+fig_pie.update_traces(textinfo="label+percent root")
+
+fig_pie.update_layout(
+    plot_bgcolor="rgba(0, 0, 0, 0)",
+    paper_bgcolor="rgba(0, 0, 0, 0)",
+    font=dict(family=BODY_FONT_FAMILY),
+    hoverlabel=dict(font=dict(family=BODY_FONT_FAMILY, color=PLOTLY_FONT_COLOR)),
     margin=dict(l=0, r=0, t=50, b=0),
     legend=dict(
         title="Account Type",
@@ -93,15 +125,29 @@ app.layout = dmc.MantineProvider(
         html.Div(
             [
                 dmc.Title("Portfolio Dashboard", order=2),
+                dmc.Group([
                 dmc.Card(
-                    [
-                        dcc.Graph(figure=fig, config=PLOTLY_CONFIG),
+                    [   dmc.Title("Total Portfolio Value over Time", order=3),
+                        dcc.Graph(figure=fig_area, config=PLOTLY_CONFIG),
                     ],
                     pt="md",
                     pb="cs",
                     shadow="sm",
                     withBorder=True,
+                    w="60%",
                 ),
+                dmc.Card(
+                    [   dmc.Title("Portfolio composition", order=3),
+                        dcc.Graph(figure=fig_pie, config=PLOTLY_CONFIG),
+                    ],
+                    p="md",
+                    shadow="sm",
+                    withBorder=True,
+                    w="30%",
+                ),
+                ],
+                ),
+
             ],
             style={"padding": "1rem"},
         ),
